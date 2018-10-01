df84d0396d1bc6505cd5c4312a24099d962f1632
Indexyz <jiduye@gmail.com>
2018-10-01 11:29:31 +0800

    fix: fix plugin name

--- plugins.sh
+++ plugins.sh
@@ -5,6 +5,6 @@ cd /var/www/usr/plugins
 
 mkdir -p GistEcho 
 wget https://gist.githubusercontent.com/legendlee/4187609/raw/fe859e9b2fb98a600cf91203b5f5fe9ac06c7ceb/GistEcho.php \
-    -O GistEcho/GistEcho.php
+    -O GistEcho/Plugin.php
 
 git clone https://github.com/MoePlayer/APlayer-Typecho.git Meting
