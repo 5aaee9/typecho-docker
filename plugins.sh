a8c4d303e0048e53fa2cfaabff4193a0450ca73b
Indexyz <jiduye@gmail.com>
2018-10-01 11:21:10 +0800

    fix: fix gistecho folder name

--- plugins.sh
+++ plugins.sh
@@ -3,8 +3,8 @@ set -e
 
 cd /var/www/usr/plugins
 
-mkdir -p gist 
+mkdir -p GistEcho 
 wget https://gist.githubusercontent.com/legendlee/4187609/raw/fe859e9b2fb98a600cf91203b5f5fe9ac06c7ceb/GistEcho.php \
-    -O gist/GistEcho.php
+    -O GistEcho/GistEcho.php
 
 git clone https://github.com/MoePlayer/APlayer-Typecho.git Meting
